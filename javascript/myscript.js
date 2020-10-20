ce8f65b5f31e5dc9c0228bf6a716dc94fe8d0d1f
Daniele <danlup94@gmail.com>
2020-10-20 15:18:40 +0200

    Commit iniziale
    inserimento struttura logica js

---
+++ javascript/myscript.js
@@ -0,0 +1,9 @@
+// ESERCIZIO: utente seleziona destinazione treno in km, prezzo 0,21/km. Sconto 20% per minorenni, 40% over 65
+
+// Utente inserisce numero di Km
+
+// Utente inserisce età
+
+// calcolo del prezzo
+
+// stampa del prezzo finale
