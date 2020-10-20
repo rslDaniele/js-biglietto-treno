50d3c54488b3ce87e1161788eca236dd85c26b61
Daniele <danlup94@gmail.com>
2020-10-20 15:44:54 +0200

    Secondo commit
    inserimento elementi prova operatori js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,8 +2,32 @@
 
 // Utente inserisce numero di Km
 
+var chilometri = prompt("Quanti chilometri percorrerai?");
+
 // Utente inserisce età
 
+var eta = prompt("Quanti anni hai?")
+
+// definizione prezzo biglietto
+
+var prezzo;
+
+prezzo = chilometri * 0.21 ;
+
+console.log(prezzo);
+
 // calcolo del prezzo
+
+if (eta < 18){
+  prezzo = (prezzo - 2 );
+} else if (eta > 65) {
+  prezzo = (prezzo + 2 );
+
+}
+
+console.log(prezzo)
+
+
+
 
 // stampa del prezzo finale
